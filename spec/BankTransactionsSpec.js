926469abbc7d74dff90002690cac6c5a89201b22
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-17 12:26:59 +0000

    Refactoring logic to save the details of the transaction

--- spec/BankTransactionsSpec.js
+++ spec/BankTransactionsSpec.js
@@ -1,35 +1,22 @@
 'use strict'
 describe("Transaction", function(){
 	var transaction;
-	var today = new Date(2019, 2, 17)
+	var amount = 1000;
+	var balance = 1000;
+	var date = new Date(2019, 2, 17)
 	beforeEach(function(){
-		transaction = new Transaction(1000, 1000, today);
+		transaction = new Transaction();
+		transaction.add(1000, 1000, date);
 	})
 	it("transaction, should be an instance", function(){
 		expect(transaction instanceof(Transaction)).toBe(true);
 	});
 
-	it("Should be defined amount", function(){
-		expect(transaction.amount).toBeDefined();
+	it("Should be defined where to save the details", function(){
+		expect(transaction.details).toBeDefined();
 	});
 
-	it("Should be defined balance", function(){
-		expect(transaction.balance).toBeDefined();
-	});
-
-	it("Should be defined date", function(){
-		expect(transaction.date).toBeDefined();
-	});
-
-	it("Should pass correctly the value from amount", function(){
-		expect(transaction.amount).toEqual(1000)
-	});
-
-	it("Should pass correctly the value from balance", function(){
-		expect(transaction.balance).toEqual(1000)
-	});
-
-	it("Should pass correctly the value from date", function(){
-		expect(transaction.date).toEqual(today)
+	it("Should contain the details from the transaction", function(){
+		expect(transaction.details[0]).toEqual([amount, balance, date])
 	});
 })
--- src/BankTransactions.js
+++ src/BankTransactions.js
@@ -1,14 +1,10 @@
 (function(exports) {
-	function Transaction(amount, balance, date = new Date()) {
-		this.TransactionHistory = [];
-		this.amount = amount;
-		this.balance = balance;
-		this.date = date;
-		this.saveIt();
+	function Transaction(history = []) {
+		this.details = history;
 	}
 
-	Transaction.prototype.saveIt = function() {
-		this.TransactionHistory.push(this.amount, this.balance, this.date);
+	Transaction.prototype.add = function(amount, balance, date = new Date()) {
+		this.details.push([amount, balance, date]);
 	};
 	exports.Transaction = Transaction;
 
